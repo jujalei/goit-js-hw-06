62375be53881715e28fe64f17d5bc202e9006b05
YuliyaYaremchuk <118767028+jujalei@users.noreply.github.com>
2023-04-14 21:48:16 +0300

    edit task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,12 @@ const nameInput = document.getElementById('name-input');
 const nameOutput = document.getElementById('name-output');
 
 nameInput.addEventListener('input', () => {
-    if (nameInput.value === ''){ 
-        nameOutput.textContent = 'Anonymous'
-    }else{
-        nameOutput.textContent = nameInput.value;
-    }
+    // if (nameInput.value === ''){ 
+    //     nameOutput.textContent = 'Anonymous'
+    // }else{
+    //     nameOutput.textContent = nameInput.value;
+    // }
+
+    nameInput.value.trim() === '' ? nameOutput.textContent = 'Anonymous' : nameOutput.textContent = nameInput.value;
+
 })
